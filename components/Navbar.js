378fdace5d35700892254001527400577d644761
SeyedMohammadHoseini <seyedmohammadhoseini114@gmail.com>
2026-05-11 10:43:22 +0330

    update project

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,19 +2,26 @@
 
 import Link from "next/link";
 import { useCartStore } from "../store/cart-store";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const items = useCartStore((state) => state.items || []);
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const count = items.reduce((s, i) => s + (i.quantity || 0), 0);
 
   return (
     <header className="sticky top-0 z-50 bg-white shadow-md">
       <div className="container px-4 py-3 flex items-center gap-4">
-        {/* لوگو */}
-        <div className="font-extrabold text-2xl text-red-600">دیجی‌کالا</div>
+        <div className="ml-auto flex items-center gap-4">
+          <div className="font-extrabold text-2xl text-red-600">دیجی‌کالا</div>
+        </div>
 
-        {/* سرچ */}
         <div className="flex-1 px-4">
           <div className="relative">
             <input
@@ -28,8 +35,7 @@ export default function Navbar() {
           </div>
         </div>
 
-        {/* منو */}
-        <div className="flex items-center gap-5 shrink-0">
+        <div className="flex items-center gap-4">
           <Link
             href="/"
             className="text-sm font-medium text-gray-700 hover:text-red-600"
@@ -37,22 +43,15 @@ export default function Navbar() {
             خانه
           </Link>
 
-          <Link
-            href="/login"
-            className="text-sm text-gray-700 whitespace-nowrap"
-          >
+          <Link href="/login" className="text-sm text-gray-700">
             ورود / ثبت‌نام
           </Link>
 
-          {/* سبد خرید */}
-          <Link
-            href="/cart"
-            className="relative flex items-center justify-center w-10 h-10 rounded-full hover:bg-gray-100 transition"
-          >
+          <Link href="/cart" className="relative text-gray-700">
             <span className="text-2xl">🛒</span>
 
-            {count > 0 && (
-              <span className="absolute -top-1 -left-1 bg-red-600 text-white rounded-full text-xs w-5 h-5 flex items-center justify-center">
+            {mounted && count > 0 && (
+              <span className="absolute -top-2 left-0 bg-red-600 text-white rounded-full text-xs w-5 h-5 flex items-center justify-center">
                 {count}
               </span>
             )}
