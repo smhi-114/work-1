eb83912d31a2443e5ce8cbca266f2199a257deb1
SeyedMohammadHoseini <seyedmohammadhoseini114@gmail.com>
2026-05-12 12:18:16 +0330

    update project

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,24 @@
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { CartProvider } from "../context/CartContext";
 
 export const metadata = {
   title: "Work-1 App",
   description: "Production ready application",
-  keywords: ["nextjs", "react", "ecommerce"],
-  openGraph: {
-    title: "Work-1 App",
-    description: "Modern optimized application",
-    type: "website",
-  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fa">
+    <html lang="fa" dir="rtl">
       <body>
-        <Navbar />
+        <CartProvider>
+          <Navbar />
 
-        <main>{children}</main>
+          <main>{children}</main>
 
-        <Footer />
+          <Footer />
+        </CartProvider>
       </body>
     </html>
   );
